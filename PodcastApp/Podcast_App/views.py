e915ce4da96b9a3dec4c689bd3de4941212b7fe0
seanmed <seanmed@bu.edu>
2020-11-07 05:22:40 -0500

    Prototype with API integration but full dictionary isn't being displayed

--- PodcastApp/Podcast_App/views.py
+++ PodcastApp/Podcast_App/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import SearchSpotifyAPIForm
-# Create your views here.
+import importlib
+from Podcast_App.spotify_client import *
+# Create your views here
+
+spotify= SpotifyAPI(client_id, client_secret)
 
 def contact(request):
     if request.method == 'POST':
         form = SearchSpotifyAPIForm(request.POST)
         if form.is_valid():
             twitter_username= form.cleaned_data['twitter_username']
-            print(twitter_username)
+
+
+
+        return HttpResponse(spotify.search(twitter_username, search_type="artist"))
 
 
     form= SearchSpotifyAPIForm()
     return render(request, 'form.html', {'form': form})
+
+
